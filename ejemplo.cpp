0faa45a0ae913170fa2213fe88771bacc4d99e3c
andresM910 <andresmarrufo.o@outlook.com>
2023-02-05 13:24:38 -0600

    actualizado

--- ejemplo.cpp
+++ ejemplo.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
  int main(){
     cout<<"primera prueba de git";
+     cout<<"primera prueba de git";
+      cout<<"primera prueba de git";
+       cout<<"primera prueba de git";
+        cout<<"primera prueba de git";
+         cout<<"primera prueba de git";
+         
 
 
  }
